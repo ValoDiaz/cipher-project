309f081b27c00ed29665bf84016b639e83698a9e
ValoDiaz <valohuxley@gmail.com>
2020-06-29 13:58:01 -0500

    first commit

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,15 @@
 window.cipher = {
-    // Acá escribe tu código. PRUEBA USER GITHUB
+    encode: (offset, string) => {
+        let result;
+        for (let i = 0; i<(string.length); i++){
+            let asciiPosition = string.charCodeAt(i);
+            let enCodexString = (asciiPosition-65 + offset) %26 + 65;
+            result = String.fromCharCode(enCodexString);
+        }
+        return result;
+    }
 };
+
+
+/*cipher.encode(offset, string): offset es el número de posiciones que queremos mover a la derecha en el alfabeto y string el mensaje (texto) que queremos cifrar.
+cipher.decode(offset, string): offset es el número de posiciones que queremos mover a la izquierda en el alfabeto y string el mensaje (texto) que queremos descifrar.*/
---
+++ src/index.js
@@ -0,0 +1,46 @@
+
+var nameClient=document.getElementById("nameClient");
+var mail= document.getElementById("mail");
+var numTel=document.getElementById("numTel");
+var direction=document.getElementById("direction");
+var encript=document.getElementById("encript");
+var result=document.getElementById("result");
+var numADesplazar=document.getElementById("numADesplazar");
+var muestraEncript= document.getElementById("muestraEncript");
+var descencriptar = document.getElementById("descencriptar");
+var textEncript= document.getElementById("textEncript");
+var textDescencript = document.getElementById("textDescencript");
+
+
+
+function encriptar () {
+    var resultCompleto = "Nombre: " + nameClient.value + " Correo: " + mail.value + " Numero: "  + numTel.value + " Dirección: " + direction.value;
+    result.innerText = resultCompleto;
+    console.log(resultCompleto);
+    let encriptado= "";
+    console.log(encriptado);
+            for (let i = 0; i<(resultCompleto.length); i++){
+            console.log(resultCompleto.length);
+            let asciiPosition = resultCompleto.charCodeAt(i);
+            console.log(asciiPosition);
+            let converNum = (asciiPosition - 32 + numADesplazar.value) %95 + 32;
+            console.log(converNum);
+            encriptado += String.fromCharCode(converNum);
+            console.log(encriptado);
+            }
+        muestraEncript.innerText = encriptado;
+        console.log(muestraEncript);
+        return encriptado;
+};
+
+function descencriptar () {
+let descifrado = "";
+    for (let i = 0; i<(encriptado.length); i++){
+    let asciiPositionDos= encriptado.charCodeAt(i);
+    console.log(asciiPositionDos);
+    let converCifrado = (asciiPositionDos - 32 - numADesplazar.value) %95 + 32;
+    descifrado += String.fromCharCode(converCifrado);
+}
+    textDescencript.innerText = descifrado;
+    return descifrado;
+};
